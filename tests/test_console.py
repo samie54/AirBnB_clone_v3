13d4fb84cd1aaac60fdfaf05c76fe0ece8e97ad2
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-30 13:17:11 -0700

    Updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py.
-
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
+"""
+Defines unittests for console.py
 """
 
 import console
